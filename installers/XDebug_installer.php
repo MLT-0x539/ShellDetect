fa6dec51b64c50fe363bde0025a8d2cc3aa33d81
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-04 16:11:50 +0000

    Create XDebug_installer.php

---
+++ installers/XDebug_installer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>XDebug installer</title>
+</head>
+<body>
+  <form action="XDebug_installer.php" method="POST">
+ Sudo credentials for authentication: <input type="text" name="sudopass"> <br />
+<input type="submit">
+</form>
+
+<?php 
+  userInput = $_POST['sudopass'];
+  if (isset($userInput)) {
+    system("su . $userInput);   // LOL.. 
+    }
+  else {
+    echo "<b><p>Sudo password not inputted</p></b>";
+  }
+?>
+
+</body>
+</html>
+
+  
