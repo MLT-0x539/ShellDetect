b2ca82044c2c5e08d9691f8648ccfec01e8b1e17
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-04 11:31:43 +0000

    Update results_sort.sh

--- results_sort.sh
+++ results_sort.sh
@@ -1,1 +1,29 @@
 // CODE FOR RESULTS SORTER TO GO HERE
+
+#!/bin/bash
+ read shell_lang
+  if [[ $shell_lang == "php" || "phtml" || "php3" || "php4" || "php5" || "MOAR TO GO HERE"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == ""asp"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "aspx"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "jsp"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "js"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "cfm"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "htaccess"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "ico"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "rb"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "pl"]]; then
+    echo -e "code to go here"
+  elif [[ $shell_lang == "cgi"]]; then
+    echo -e "code to go here"
+else 
+   echo -e "language not yet detected or supported"
+fi
