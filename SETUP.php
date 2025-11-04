dcb246188480c09290ee4be148e1f52fbc608028
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-11-04 12:53:53 +0000

    Update log directory creation logic in SETUP.php
    Fix syntax errors and improve log directory creation.

--- SETUP.php
+++ SETUP.php
@@ -1,7 +1,21 @@
 <?php
 error_reporting(0);
+$current_date = date("Y-m-d");
+$current_time = time();
+$datetime = $current_date . $current_time;
+$logdate = "echo $datetime";
+$logpath = "/result_logz/$logdate/log.html";
+$webroot = get_current_user();
 
 include("INIT.php");
 getVersionz();
+
+if (!file_exists($logpath) {
+  chown($webroot . $logpath);
+  mkdir($logpath);
+}
+else {
+throw new Exception(logpath . "file already exist");
+}
 
 ?>
